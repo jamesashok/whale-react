0c01cbad0605616f245f3304f670987739cab028
James Ashok <jamesashok93@gmail.com>
2023-08-14 20:25:03 +0530

    React quickstart - initial.

---
+++ app/whale-app.tsx
@@ -0,0 +1,10 @@
+import { WhaleButton } from "./whale-button";
+
+export default function WhaleApp() {
+  return (
+    <div>
+      <h1>Whale React Next</h1>
+      <WhaleButton />
+    </div>
+  );
+};
---
+++ app/whale-button.tsx
@@ -0,0 +1,3 @@
+export function WhaleButton() {
+  return <button className="btn">Click</button>;
+}
