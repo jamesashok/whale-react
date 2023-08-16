9fc68f5c7f79ed511b8374fba626c18b80710294
James Ashok <jamesashok93@gmail.com>
2023-08-16 19:28:01 +0530

    React JSX, data, conditional rendering, lists, events, state.

--- app/whale-button.tsx
+++ app/whale-button.tsx
@@ -1,3 +1,19 @@
+"use client";
+
+import { useState } from "react";
+
 export function WhaleButton() {
-  return <button className="btn">Click</button>;
+  const buttonConfig = {
+    label: "Click",
+    class: "btn",
+  };
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  return (
+    <button className={buttonConfig.class} onClick={handleClick}>
+      {buttonConfig.label} - Counter {count}
+    </button>
+  );
 }
