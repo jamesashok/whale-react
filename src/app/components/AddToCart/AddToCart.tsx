9a7e5861febf0db092fcc095cfb4f42b0667d01d
James Ashok <jamesashok93@gmail.com>
2024-01-16 17:28:53 +0530

    Client rendered component.

---
+++ src/app/components/AddToCart/AddToCart.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <button
+      onClick={() => {
+        console.log("Add to cart");
+      }}
+    >
+      Add to Cart
+    </button>
+  );
+};
+
+export default AddToCart;
