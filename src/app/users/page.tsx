0a0857f72a6faf53d013c2ffb10f3c6c2aa97d2f
James Ashok <jamesashok93@gmail.com>
2024-01-17 10:50:21 +0530

    React Suspense on RSC.

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,28 +1,36 @@
 import Link from "next/link";
-import React from "react";
+import React, { Suspense } from "react";
 
 interface User {
   id: number;
   name: string;
 }
 
-const UsersPage = async () => {
+async function UserList() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users", {
     cache: "no-store",
   });
   const users: User[] = await res.json();
 
   return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
+}
+
+const UsersPage = async () => {
+  return (
     <main>
       <Link href="/">Home Page</Link> |{" "}
       <Link href="/users/new">New Users Page</Link>
       <h1>Users</h1>
       <p>{new Date().toLocaleTimeString()}</p>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <UserList />
+      </Suspense>
     </main>
   );
 };
